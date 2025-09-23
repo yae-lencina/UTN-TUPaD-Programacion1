77365ee71903a99c539ce752880e758bf8b23cb0
yael <yaellencina@gmai.com>
2025-09-23 06:24:11 -0300

    Ejercicio 1

---
+++ unidad-2/ejercicio-1.py
@@ -0,0 +1,6 @@
+#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”
+edad=int(input("Ingrese su edad: "))
+
+if(edad>=18):
+    print("Ud es mayor de edad.")

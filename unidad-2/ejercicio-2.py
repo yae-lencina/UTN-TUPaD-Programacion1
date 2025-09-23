15302202ecbb3b29b7dfe53b33bd1f21ed3904fe
yael <yaellencina@gmai.com>
2025-09-23 06:30:51 -0300

    Ejercicio2
    hicimos ejercicio

---
+++ unidad-2/ejercicio-2.py
@@ -0,0 +1,9 @@
+#2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+#mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+#mensaje “Desaprobado”.
+
+Nota=int(input("Imgrese nota: "))
+if(Nota>=6):
+    print("Aprobado.")
+else:
+    (print("Desaprobado."))

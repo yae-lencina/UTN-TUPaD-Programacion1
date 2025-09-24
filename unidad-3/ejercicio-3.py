2d3d11541f3330a31f12db2707ad2a4f025225ae
yael <yaellencina@gmai.com>
2025-09-24 11:35:19 -0300

    correccion ej 3

--- unidad-3/ejercicio-3.py
+++ unidad-3/ejercicio-3.py
@@ -2,11 +2,10 @@ numero=int(input("Ingrese un numero: "))
 numero2=int(input("Ingrese otro numero: "))
 
 if (numero<numero2):
-    sumatoria=numero
-    for i in range (numero+1,numero2 +1):
+    sumatoria=0
+    for i in range (numero+1,numero2):
         print(sumatoria)
         sumatoria = sumatoria+i
-        #print(sumatoria)
-#sumatoria= sumatoria+numero2
+        
 
 print("La sumatoria entre los numeros ", numero,  " y ", numero2 , " es: ", sumatoria)

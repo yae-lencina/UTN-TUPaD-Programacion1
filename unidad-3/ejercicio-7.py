e052254b6e717206b0e09c10ed89585945fec4f9
yael <yaellencina@gmai.com>
2025-09-26 07:41:00 -0300

    Ejercicio 7
    Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
    número entero positivo indicado por el usuario.

---
+++ unidad-3/ejercicio-7.py
@@ -0,0 +1,9 @@
+numero=int(input("Ingrese un numero: "))
+
+sumatoria=0
+for i in range (0,numero,+1):
+    print(sumatoria)
+    sumatoria = sumatoria+i
+        
+
+print("La sumatoria entre 0 el numero ", numero, " es: ", sumatoria)
